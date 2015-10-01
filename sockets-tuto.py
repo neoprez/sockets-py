36a932d29b918540dcfcf3e7a39485a735dd266e
Rodny Perez <rodnyeperez@gmail.com>
2015-09-30 22:10:03 -0400

    Add error handling of errors

--- sockets-tuto.py
+++ sockets-tuto.py
@@ -1,8 +1,14 @@
-# Socket client example in python
+# handling errors in python socket programs
 
 import socket #for sockets
+import sys #for exit
 
-#create an AF_INET, STREAM socket (TCP)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+	#create an AF_INET, STREAM socket (TCP)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except Exception as err:
+	code, msg = inst.args
+	print('Failed to create socket. Error code: ' + str(code) + ', Error message : ' + msg)
+	sys.exit();
 
 print('Socket Created')
