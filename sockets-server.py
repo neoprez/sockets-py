2f77c6553d580a195e6347427ab02c3d98a2a1b6
Rodny Perez <rodnyeperez@gmail.com>
2015-09-30 22:43:28 -0400

    Add sockets server

---
+++ sockets-server.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+
+HOST = '' # Symbolic name meaning all available interfaces
+PORT = 8888 #Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+
+try:
+	s.bind((HOST,PORT))
+except Exception as err:
+	code, msg = err.args
+	print("Bind failed. Error code: " + str(code) + ' msg: ' + msg)
+	sys.exit()
+
+print('Socket bind complete')
