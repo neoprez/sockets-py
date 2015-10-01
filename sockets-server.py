f2e042907062fa27fd921089bfe2a81ec53aaa8d
Rodny Perez <rodnyeperez@gmail.com>
2015-09-30 23:14:33 -0400

    Handle multiple connections

--- sockets-server.py
+++ sockets-server.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import threading
 
 HOST = '' # Symbolic name meaning all available interfaces
 PORT = 8888 #Arbitrary non-privileged port
@@ -19,19 +20,33 @@ print('Socket bind complete')
 s.listen(10)
 print("Socket now listening")
 
+# Function for handling connections. This will be used to create threads
+def clientthread(conn):
+	#Sending message to connected client
+	conn.send(b'Welcome to the server. Type something and hit enter\n')
+	
+	#Infinte loop so that the function do not terminate and do not end.
+	while True:
+		
+		#Receiving from client
+		data = conn.recv(1024)
+		reply = b'Ok...' + data
+		if not data:
+			break
+
+		conn.sendall(reply)
+
+	#come out of loop
+	conn.close()
+
 # now keep talking with the client
 while 1:
 	#wait to accept a connection - blocking call
 	conn, addr = s.accept()
 	print('Connected with ' + addr[0] + ':' + str(addr[1]))
+	
+	#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function
+	threading.Thread(target=clientthread,args=(conn,)).start()
+	#start_new_thread(clientthread, (conn,))
 
-	#now keep talking with the client
-	data = conn.recv(1024)
-	reply = b'Ok..'+ data
-	if not data:
-		break
-
-	conn.sendall(reply)
-
-conn.close()
 s.close()
